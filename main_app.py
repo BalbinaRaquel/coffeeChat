01ddbf51af14bf8477a0d2dbf1d81413561304c5
BalbinaRaquel <balbina.rakell@outlook.com>
2024-10-21 17:34:49 -0300

    Entrar e iniciar chat

--- main_app.py
+++ main_app.py
@@ -11,7 +11,7 @@
         # 1.2.1 Título do popup/alerta: 'Bem-vindo!'
         # 1.2.2 Caixa de texto: 'Escreva seu nome no chat'
         # 1.2.3 Botão: 'Entrar no chat'
-    # 1.3 Ao clicar no botão -> remover título - remover botão 'Iniciar chat'
+    # 1.3 Ao clicar no botão -> remover título - remover botão 'Iniciar chat' e fechar popup
         # 1.3.1 Carregar chat;
         # 1.3.2 Carregar campo de enviar mensagem: 'Digite sua mensagem'
         # 1.3.3 Carregar o botão 'enviar' mensagem
@@ -34,20 +34,66 @@ import flet as ft
 def main(pagina):
     # steps do projeto
 
-
+    
+    
     imagem_pg = ft.Image(src=f'icon1.png', width=250, height=250, fit=ft.ImageFit.CONTAIN,)
     pagina.add(imagem_pg)
     # Criar Título  
-    titulo = ft.Text('Coffee & Chat')
+    titulo = ft.Text('Bem-vindos ao Coffee & Chat!')
     pagina.add(titulo)
 
+    
+
+    campo_enviar_mensagem = ft.TextField(label='Digite a sua mensagem...')
+    botao_enviar = ft.ElevatedButton('Enviar')
+    
+    linha_mensagem = ft.Row([campo_enviar_mensagem, botao_enviar])
+
+    chat = ft.Column()
+
+
+    # criar função entrar no chat com o click do botão do popup
+    # Ao clicar no botão -> remover título - remover botão 'Iniciar chat' e fechar popup
+    def entrar_chat(evento):
+
+        # fechar popup
+        popup.open = False
+        # remover título
+        pagina.remove(titulo)
+        # Remover botão iniciar chat
+        pagina.remove(botao_iniciar)
+        
+        # Carregar o chat
+        pagina.add(chat)
+        
+        # carregar o campo de enviar mensagem e carregar botão enviar
+        pagina.add(linha_mensagem)
+        pagina.update()
+        
+
+
+    # criar popup que abre ao criar no botão iniciar
+    titulo_popup = ft.Text('Vamos iniciar o Coffeebreak?')
+    caixa_nome = ft.TextField(label='Digite o seu nome')
+    botao_popup = ft.ElevatedButton('Entrar no Chat', on_click=entrar_chat)
+    
+    popup = ft.AlertDialog(title=titulo_popup, content=caixa_nome, actions=[botao_popup])
+
+
+
     # Criar botão iniciar
-    botao_iniciar = ft.ElevatedButton('Iniciar Chat')
+    def abrir_popup(evento):
+        # ação que executa ao clicar no botão
+        pagina.dialog = popup
+        popup.open= True
+        pagina.update()
+    
+    botao_iniciar = ft.ElevatedButton('Iniciar Chat', on_click=abrir_popup)
     pagina.add(botao_iniciar)
 
 
 
 # Executar a função main com o flet
-ft.app(main, view=ft.W)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=8000)
 
 
